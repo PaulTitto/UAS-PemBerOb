bffcd4aecfc0ff4d04cdafd7197d2d419f9d7314
sheilanuraeniafidah <sheilanuraeni77@gmail.com>
2023-06-25 11:02:57 +0700

    Create Varian2.java

---
+++ RotiHanari/src/PackInterface/Varian2.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package PackInterface;
+
+/**
+ *
+ * @author user
+ */
+public interface Varian2 {
+    public int jumlahPesan();
+}
